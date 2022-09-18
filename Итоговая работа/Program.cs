06d51237e495117540f3a3f6f49b1fadd9fcab3c
YersiniaPestis1989 <azentsova@list.ru>
2022-09-19 00:29:41 +0700

    Add 2 var program

--- Итоговая работа/Program.cs
+++ Итоговая работа/Program.cs
@@ -1,19 +1,13 @@
-﻿Console.Write("Введите количество элементов массива ");
+﻿
+
+Console.Write("Введите количество элементов массива ");
 int lenght = Convert.ToInt32(Console.ReadLine());
 Console.Clear();
 string[] massive = new string[lenght];
 FillArray(massive);
 PrintArray(massive);
-int count = 0;
-  for (int i = 0; i < lenght; i++)
-    {
-      if (massive[i].Length <= 3)
-                    count++;
-    }
-Console.WriteLine("Чисел в массиве, удовлетворяющих условию, всего: " + count);
-string[] massive2 = new string[count];
-FillSecondArrayWithIF(massive, massive2);
-PrintArray(massive2);
+PrintWithIF(massive);
+
 void FillArray(string[] array)
             {
                 for (int i = 0; i < array.Length; i++)
@@ -31,14 +25,14 @@ void PrintArray(string[] array)
                 }
                 Console.WriteLine();
             }
-void FillSecondArrayWithIF(string[] array1, string[] array2)
+void PrintWithIF(string[] array1)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
+        Console.Write($"{array1[i]} ");
         count++;
         }
     }
